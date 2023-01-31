07e4eac5d93cc9fbf04553e3d4ebf1c52f7adc45
SalehAlzahrani94 <110546933+SalehAlzahrani94@users.noreply.github.com>
2023-01-31 10:36:09 -0800

    Add files via upload

---
+++ degrees.py
@@ -0,0 +1,180 @@
+import csv
+import sys
+
+from util import Node,StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+    # in this part i start with frontierPage that has initial state
+    start =Node(state=source,parent=None,action=None)
+    frontierPage = QueueFrontier() # note this is breadth search
+    frontierPage.add(start)
+    # add a start to our set
+    #start with empty set
+    explored = set()
+    # in this part use a loop to keep looking
+    while True:
+
+        if frontierPage.empty(): # if our frontier empty return none
+            return None
+           # take a node from our frontier page
+        node= frontierPage.remove()
+
+        if node.state == target: # in this part if condition to check if the node is the target we looking for
+            rt = []
+            while node.parent is not None:
+                rt.append((node.action, node.state))
+                node = node.parent
+
+            rt.reverse() # erverse function to take last opject  we add
+            return rt
+        explored.add(node.state )
+
+        #this part for findong the  neighbour (like look from A to B  ,A to C)
+        neighbours= neighbors_for_person( node.state )
+
+        # in this part we add neighbour we explore to our frontierPage
+        for action, state in neighbours:
+            if not frontierPage.contains_state(state) and state not in explored : # look for our fronter page if its contains state
+                child = Node(state=state ,parent=node ,action=action )
+                if child.state ==target : # if condition  true ( the state is the target we are looking for )
+                    rt= []
+
+                    while child.parent is not None :
+                        rt.append((child.action, child.state)) # adding
+                        child = child.parent # chaning child ( still working )
+                    rt.reverse() # change and arrange the items
+                    return rt
+                frontierPage.add(child) # in here add the child we find
+
+    # TODO
+    raise NotImplementedError # its a default
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
